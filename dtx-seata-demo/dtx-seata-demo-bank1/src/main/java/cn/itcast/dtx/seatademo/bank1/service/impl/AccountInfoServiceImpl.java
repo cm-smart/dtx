323ec4985cda3f2f13b277bf78e7b46037c6e3ef
chenmin <1459422492@qq.com>
2023-05-25 15:20:23 +0800

    使用seate实现分布式事务

---
+++ dtx-seata-demo/dtx-seata-demo-bank1/src/main/java/cn/itcast/dtx/seatademo/bank1/service/impl/AccountInfoServiceImpl.java
@@ -0,0 +1,39 @@
+package cn.itcast.dtx.seatademo.bank1.service.impl;
+
+import cn.itcast.dtx.seatademo.bank1.dao.AccountInfoDao;
+import cn.itcast.dtx.seatademo.bank1.service.AccountInfoService;
+import cn.itcast.dtx.seatademo.bank1.spring.Bank2Client;
+import io.seata.core.context.RootContext;
+import io.seata.spring.annotation.GlobalTransactional;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class AccountInfoServiceImpl implements AccountInfoService {
+
+    @Autowired
+    private AccountInfoDao accountInfoDao;
+
+    @Autowired
+    private Bank2Client bank2Client;
+
+    @GlobalTransactional//开启全局事务
+    @Override
+    public void updateAccountBalance(String accountNo, Double amount) {
+        log.info("bank1 service begin,XID：{}", RootContext.getXID());
+        //扣减张三的金额
+        accountInfoDao.updateAccountBalance(accountNo,amount *-1);
+        //调用李四微服务，转账
+        String transfer = bank2Client.transfer(amount);
+        if("fallback".equals(transfer)){
+            //调用李四微服务异常
+            throw new RuntimeException("调用李四微服务异常");
+        }
+        if(amount == 2){
+            //人为制造异常
+            throw new RuntimeException("bank1 make exception..");
+        }
+    }
+}
